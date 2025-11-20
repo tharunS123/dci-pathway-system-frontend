61c7f490c914fba838ad5b28f7032ddd384d8f3b
tharunS123 <tharunsenthilkumar308@gmail.com>
2025-11-20 14:27:53 -0600

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ArrowUpRight, Badge, CirclePlay } from "lucide-react";
 import Header from "@/components/Header";
+import { Footer2 } from "@/components/Footer";
 
 function Home() {
   return (
     <>
       <Header />
+      <Footer2 />
     </>
   );
 }
